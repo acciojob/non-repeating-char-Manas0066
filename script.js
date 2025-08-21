b861c80a99c90ac88cac81771a1688c5964303cb
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-08-22 03:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	let charcnt = {};
+
+	for(let char of str){
+		charcnt[char] = 1+ (charcnt[char]||0);
+	}
+
+	for(let ch of charcnt){
+		if(charcnt[ch]===1){
+			return ch;
+		}
+	}
+
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
